b2fc1031048219c2baa093ac60ec2cb6de057a4e
Andres <andresycristi9091@gmail.com>
2026-06-10 18:12:00 -0400

    fix: forzar login como dinamico para evitar fallo de prerender en Vercel

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,7 @@
 import AgendaSaludLoginPage from '@/components/AgendaSaludLoginPage'
 
+export const dynamic = 'force-dynamic'
+
 export default function LoginPage() {
   return <AgendaSaludLoginPage />
 }
